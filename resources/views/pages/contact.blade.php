f6261adac4f0292ff2f1a362e220fded7c2073ff
anupama <ak2522@njit.edu>
2018-10-23 02:08:55 -0400

    Task: Added jumbotron.css file

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,7 +1,29 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: anupama
- * Date: 10/23/18
- * Time: 12:47 AM
- */
+@extends('layouts.default')
+
+@section('content')
+
+    <div class="card mt-3 pl-2 pr-2">
+        <div class="card-title">
+            <h1>Contact</h1>
+            <p class="lead">Use this Contact form to access our customer services</p></div>
+        
+        <div class="card-body">
+            <form action="/contact" method="post">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
+                </div>
+
+                <div class="form-group">
+                    <label for="body">Message</label>
+                    <textarea name="body" class="form-control" id="body" rows="3"></textarea>
+                </div>
+
+                <button type="submit" class="btn btn-primary mb-2">Submit</button>
+            </form>
+        </div>
+    </div>
+
+
+@endsection
