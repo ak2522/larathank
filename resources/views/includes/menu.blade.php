d50d1f106dde13fb1d95f609bde88f6e06353c53
anupama <ak2522@njit.edu>
2018-10-23 09:25:53 -0400

    Feature: Add Changes to menu.blade.php

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -1,10 +1,4 @@
-
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <nav class="navbar navbar-default">
+<nav class="navbar navbar-default">
         <div class="container-fluid">
     <div class="navbar-header">
         <a class="navbar-brand" href="#">Laravel</a>
@@ -14,12 +8,8 @@
             <li><a href="{{route('about')}}">About</a></li>
             <li><a href="{{route('contact')}}">Contact</a></li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
     </div>
     </nav>
-</nav>
+
 
 
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@
 Route::get('/', function(){
     return view('pages.home');
 })->name('home');
-Route::get('/', function(){
+Route::get('/about', function(){
     return view('pages.about');
 })->name('about');
-Route::get('/', function(){
+Route::get('/contact', function(){
     return view('pages.contact');
 })->name('contact');
