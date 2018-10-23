f79a1138cf223ec120e4a27fbf24475921e2b6fa
anupama <ak2522@njit.edu>
2018-10-23 09:34:31 -0400

    Feature: Changes to web.php in Routes

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
-Route::get('/', function(){
-    return view('pages.home');
-})->name('home');
-Route::get('/about', function(){
-    return view('pages.about');
-})->name('about');
-Route::get('/contact', function(){
-    return view('pages.contact');
-})->name('contact');
+*/;
+Route::get('/', 'PagesController@home')->name('home');
+Route::get('/about', 'PagesController@about')->name('about');
+Route::get('/contact', 'PagesController@contact')->name('contact');
