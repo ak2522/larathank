806583bdc81da4d80c6fa6f570dafc3080581c4a
anupama <ak2522@njit.edu>
2018-10-23 09:16:28 -0400

    Feature: Changes to menu.blade.php

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -4,18 +4,22 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+    <div class="navbar-header">
+        <a class="navbar-brand" href="#">Laravel</a>
+    </div>
         <ul class="nav navbar-nav">
             <li class="active"><a href="{{route('home')}}">Home</a></li>
             <li><a href="{{route('about')}}">About</a></li>
             <li><a href="{{route('contact')}}">Contact</a></li>
-
         </ul>
         <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
     </div>
+    </nav>
 </nav>
 
 
